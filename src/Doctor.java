469374807d9a43c965049124d0addc45c230a292
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-13 13:14:59 -0500

    Main files of the project were created. Doctor class contains the id, name, speciality attributes and showName method.

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    // Atributos
+    int id;
+    String name;
+    String speciality;
+
+    // Métodos
+    public void showName(){
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args){
+        Doctor myDoctor = new Doctor(); // Creación del objeto
+        myDoctor.name = "Alejandro Rodríguez"; // Asignando atributo
+        myDoctor.showName(); // Utilizando método
+    }
+}
