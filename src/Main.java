e7313a7085f542903235c9e07dc309984694537a
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-14 15:49:22 -0500

    The object myDoctor was created with the new constructor.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 import static ui.UIMenu.*;
 
 public class Main {
-    public static void main (String[] args){
-        Doctor myDoctor = new Doctor(); // Creación del objeto
-        myDoctor.name = "Alejandro Rodríguez"; // Asignando atributo
-        myDoctor.showName(); // Utilizando método
-        myDoctor.showId();
-        System.out.println(Doctor.id);
+    public static void main(String[] args) {
+        // showMenu();
 
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-        showMenu();
+        // Constructor de objeto con parámetros obligatorios
+        Doctor myDoctor = new Doctor("Gonzalo Martínez", "Pediatría");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 
 }
