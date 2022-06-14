ba868e04983c14c1a23b3cf88cb89cad0c6245ce
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-14 15:47:26 -0500

    The "Patient" class was created. The class contains a constructor anda variables.

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,24 @@
 public class Doctor {
     // Atributos
-    int id;
+    static int id=0; //Autoincrement
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
     // Métodos
+    Doctor(String name, String speciality){
+        id++;
+        System.out.println("El nombre del Doctor asignado es: "+name);
+        this.name = name;
+        this.speciality = speciality;
+    }
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 }
