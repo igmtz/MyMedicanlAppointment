5c3719ef8139ebd8edab538aad3b3eb540dd1436
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-14 14:09:20 -0500

    The "Main" file contains the creation of two Doctor objects using the default constructor method, assigns and shows the name, and shows the doctor id. Also has the showMenu methods from the UIMenu package.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main (String[] args){
         Doctor myDoctor = new Doctor(); // Creación del objeto
         myDoctor.name = "Alejandro Rodríguez"; // Asignando atributo
         myDoctor.showName(); // Utilizando método
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
     }
+
 }
