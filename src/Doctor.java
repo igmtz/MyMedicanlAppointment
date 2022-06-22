e9f7ab0136cfaea3b5cfb2cc8f313d7dbf236c91
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-22 13:42:33 -0500

    The AvailableAppointment nested class was added to the Doctor class, to generate the available appointments of the doctors. The variables id, date and time were added as well as their respective getters and setters. An array with ArrayList was created to store the appointments as well as the add and get appointment methods.

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     // Atributos
-    static int id=0; //Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private static int id=0; //Autoincrement
+    private String name;
+    private String email;
+    private String speciality;
+
+
+
 
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
@@ -21,5 +27,49 @@ public class Doctor {
     }
     public void showId(){
         System.out.println("ID Doctor: "+id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointment = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointment.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public  ArrayList<AvailableAppointment> getAvailableAppointment(){
+        return availableAppointment;
+    }
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
